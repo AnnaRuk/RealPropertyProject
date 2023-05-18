83ca99fe852d34c2679bfdb093dd514645c87c43
Anna Bieliaieva <belyaevaan80@gmail.com>
2023-05-18 19:13:47 +0200

    initial commit

---
+++ src/RealProperty.java
@@ -0,0 +1,10 @@
+public class RealProperty {
+
+   private String cadastralNumber; //кадастровый номер
+   private double price;
+   private Person owner; ///собственник
+    private double area; //площадь
+}
+
+
+
